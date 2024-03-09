c8795d429d1e717520ec2f88323c503915c4ff3f
Kopchan <83940856+Kopchan@users.noreply.github.com>
2024-03-09 17:22:47 +0700

    Работа с альбомами

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\ApiException;
 use App\Models\Album;
 use App\Models\Picture;
 use Carbon\Carbon;
@@ -13,103 +14,102 @@ use Illuminate\Support\Str;
 class AlbumController extends Controller
 {
     public function root() {
-        $files  = Storage::files      ('public/images');
-        $folders = Storage::directories('public/images');
-
-        $pictures = array_filter($files, function ($file) {
-            $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
-            $extension = pathinfo($file, PATHINFO_EXTENSION);
-            return in_array($extension, $allowedExtensions);
-        });
-        $parentAlbum = Album::where('path','/')->first();
-        if (!$parentAlbum)
-            $parentAlbum =  Album::create([
-                'name'=>"",
-                'path'=>"/",
-                'hash'=>Hash::make(Str::random(255)),
-            ]);
-        $picturesResponse = [];
-        foreach ($pictures as $picture){
-            $pictureModel = Picture::where('name', basename($picture))->first();
-            if(!$pictureModel)
-                $pictureModel = Picture::create([
-                    'name'=>basename($picture),
-                    'weight'=>Storage::size($picture),
-                    'date'=>Carbon::createFromTimestamp(Storage::lastModified($picture)),
-                    'hash'=>md5(Storage::get($picture)),
-                    'album_id'=>$parentAlbum->id
-                ]);
-            $picturesResponse[] = $pictureModel;
-        }
-        $albumResponse = [];
-        foreach ($folders as $folder){
-            $path = $parentAlbum->path.basename($folder);
-            $albumModel = Album::where('path', $path)->first();
-            if(!$albumModel)
-                $albumModel = Album::create([
-                    'name'=>basename($folder),
-                    'path'=>$path,
-                    'hash'=>Hash::make(Str::random(255)),
-                    'parent_album_id'=>$parentAlbum->id
-                ]);
-            $albumResponse[] = $albumModel;
-        }
-        return response()->json([
-            'content' => [
-                'albums'   => $albumResponse,
-                'pictures' => $picturesResponse,
-            ]
-        ]);
-
+        return $this->get();
     }
-    public function get(string $hash) {
-        $files  = Storage::files      ('public/images');
-        $folders = Storage::directories('public/images');
+    public function rootImages(int $page = 1) {
+        return $this->getImages(page: $page);
+    }
+    public function get(string $hash = 'root') {
+        if ($hash == 'root') {
+            $parentAlbum = Album::where('path', '/')->first();
+            if (!$parentAlbum)
+                $parentAlbum =  Album::create([
+                    'name' => '',
+                    'path' => '/',
+                    'hash' => Str::random(25),
+                ]);
+        }
+        else {
+            $parentAlbum = Album::where('hash', $hash)->first();
+            if (!$parentAlbum)
+                throw new ApiException(404, "Album with \"$hash\" hash not found");
+        }
+        $path = "images$parentAlbum->path";
+        $folders = Storage::directories($path);
+
+        $albumResponse = [];
+        foreach ($folders as $folder) {
+            $path = $parentAlbum->path.basename($folder);
+            $albumModel = Album::where('path', $path)->first();
+            if(!$albumModel)
+                $albumModel = Album::create([
+                    'name' => basename($folder),
+                    'path' => $path,
+                    'hash' => Str::random(25),
+                    'parent_album_id' => $parentAlbum->id
+                ]);
+            $albumResponse[] = $albumModel;
+        }
+        return response([
+            'albums' => $albumResponse,
+        ]);
+    }
+
+    public function getImages(string $hash = 'root', int $page = 1) {
+        if ($hash == 'root') {
+            $parentAlbum = Album::where('path', '/')->first();
+            if (!$parentAlbum)
+                $parentAlbum =  Album::create([
+                    'name' => '',
+                    'path' => '/',
+                    'hash' => Str::random(25),
+                ]);
+        }
+        else {
+            $parentAlbum = Album::where('hash', $hash)->first();
+            if (!$parentAlbum)
+                throw new ApiException(404, "Album with '$hash' hash not found");
+        }
+        $path = "images$parentAlbum->path";
+        $files = Storage::files($path);
+
+        $searchedTags = null;
+        $tagsString = request()->input('tags');
+        if ($tagsString)
+            $searchedTags = explode(',', $tagsString);
+
+        $allowedSorts = ['name', 'date', 'size'];
+        $sortType = (request()->input('sort'));
+        if (!in_array($sortType, $allowedSorts))
+            $perPage = $allowedSorts[0];
+
+        $perPage = intval(request()->input('per_page'));
+        if (!$perPage)
+            $perPage = 30;
+
+        // TODO: Сделать фильтрацию по тегам, сортировку и страницизацию через данные в базе
 
         $pictures = array_filter($files, function ($file) {
             $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
             $extension = pathinfo($file, PATHINFO_EXTENSION);
             return in_array($extension, $allowedExtensions);
         });
-        $parentAlbum = Album::where('hash',$hash)->first();
-        if (!$parentAlbum)
-            $parentAlbum =  Album::create([
-                'name'=>"",
-                'path'=>"/",
-                'hash'=>Hash::make(Str::random(255)),
-            ]);
+
         $picturesResponse = [];
         foreach ($pictures as $picture){
             $pictureModel = Picture::where('name', basename($picture))->first();
             if(!$pictureModel)
                 $pictureModel = Picture::create([
-                    'name'=>basename($picture),
-                    'weight'=>Storage::size($picture),
-                    'date'=>Carbon::createFromTimestamp(Storage::lastModified($picture)),
-                    'hash'=>Hash::make(Storage::get($picture)),
-                    'album_id'=>$parentAlbum->id
+                    'name'      => basename($picture),
+                    'weight'    => Storage::size($picture),
+                    'date'      => Carbon::createFromTimestamp(Storage::lastModified($picture)),
+                    'hash'      => md5(Storage::get($picture)),
+                    'album_id'  => $parentAlbum->id
                 ]);
             $picturesResponse[] = $pictureModel;
         }
-        $albumResponse = [];
-        foreach ($folders as $folder){
-            $path = $parentAlbum->path.basename($folder);
-            $albumModel = Album::where('path', $path)->first();
-            if(!$albumModel)
-                $albumModel = Album::create([
-                    'name'=>basename($folder),
-                    'path'=>$path,
-                    'hash'=>Hash::make(Str::random(255)),
-                    'parent_album_id'=>$parentAlbum->id
-                ]);
-            $albumResponse[] = $albumModel;
-        }
-        return response()->json([
-            'content' => [
-                'albums'   => $albumResponse,
-                'pictures' => $picturesResponse,
-            ]
+        return response([
+            'pictures' => $picturesResponse,
         ]);
-
     }
 }
