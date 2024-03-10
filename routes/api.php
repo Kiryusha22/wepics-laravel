4dba1ab2705f8c4cf2e90f27a6ebe24c45d6e6f3
Kopchan <83940856+Kopchan@users.noreply.github.com>
2024-03-11 03:10:12 +0700

    Добавление новых путей

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\ImageController;
+use App\Http\Controllers\AccessController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,36 +17,56 @@ use App\Http\Controllers\ImageController;
 |
 */
 
-Route::group([
-    'controller' => UserController::class,
-    'prefix' => 'auth'
-],function (){
+Route
+::controller(UserController::class)
+->prefix('users')
+->group(function () {
     Route::middleware('token.auth')
         ->get  ('logout', 'logout');
-    Route::post('login',  'login' );
-    Route::post('reg'  ,  'reg'   );
+    Route::post('login' , 'login' );
+    Route::post('reg'   , 'reg'   );
+
+    Route::middleware('token.auth:admin')->group(function () {
+        Route::post  (''    , 'create' );
+        Route::get   (''    , 'showAll');
+        Route::get   ('{id}', 'show'   )->where('id', '[0-9]+');;
+        Route::delete('{id}', 'destroy')->where('id', '[0-9]+');
+    });
 });
 
-Route::group([
-    'controller' => ImageController::class,
-    'prefix' => 'images'
-],function (){
-    Route::get('{hash}',      'show' );
-    Route::get('{hash}/orig', 'orig' );
-    Route::get('{hash}/thumb/{orient}/{px}', 'thumb')
-        ->where('orient', '[whWH]')
-        ->where('px'    , '[0-9]+');
-});
+Route::middleware('token.auth:guest')->group(function () {
+    Route
+    ::controller(AlbumController::class)
+    ->prefix('albums')
+    ->group(function () {
+        Route::get   ('',                      'root');
+        Route::get   ('images/{page?}',        'rootImages');
+        Route::get   ('{hash}',                'get');
+        Route::get   ('{hash}/images/{page?}', 'getImages');
+        Route::post  ('{hash}',                'create');
+        Route::post  ('{hash}/images/{page?}', 'uploadImages');
+        Route::delete('{hash}',                'destroy');
 
-Route::middleware('token.auth')->group(function () {
-    Route::group([
-        'controller' => AlbumController::class,
-        'prefix' => 'albums'
-    ],function (){
-        Route::get('',                      'root');
-        Route::get('images/{page?}',        'rootImages');
-        Route::get('{hash}',                'get');
-        Route::get('{hash}/images/{page?}', 'getImages');
+        Route
+        ::controller(AccessController::class)
+        ->prefix('access')
+        ->group(function () {
+            Route::get   ('', 'showAll');
+            Route::post  ('', 'create');
+            Route::delete('', 'destroy');
+        });
+    });
+
+    Route
+    ::controller(ImageController::class)
+    ->prefix('images')
+    ->group(function () {
+        Route::get('{hash}',      'show' );
+        Route::get('{hash}/orig', 'orig' );
+        Route::get('{hash}/thumb/{orient}/{px}', 'thumb')
+            ->where('orient', '[whWH]')
+            ->where('px'    , '[0-9]+');
+        Route::delete('{hash}', 'destroy');
     });
 });
 
