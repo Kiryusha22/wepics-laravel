9f77228754938046739bd520306be6c6e4d14b29
Kopchan <83940856+Kopchan@users.noreply.github.com>
2024-03-14 10:53:50 +0700

    WIP

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -7,18 +7,20 @@ use App\Enums\SortTypesEnum;
 use App\Exceptions\ApiException;
 use App\Http\Requests\AlbumImagesRequest;
 use App\Http\Requests\FilenameCheckRequest;
+use App\Http\Requests\UploadRequest;
 use App\Http\Resources\ImageResource;
 use App\Models\Album;
 use App\Models\Image;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Validator;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
 use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
-    public function indexingImages($album)
+    public function indexingImages($album): void
     {
         $path = "images$album->path";
         $files = Storage::files($path);
@@ -35,7 +37,7 @@ class ImageController extends Controller
             if (!$imageModel) {
                 $sizes = getimagesize(Storage::path($file));
 
-                $imageModel = Image::create([
+                Image::create([
                     'name' => basename($file),
                     'hash' => md5(Storage::get($file)),
                     'date' => Carbon::createFromTimestamp(Storage::lastModified($file)),
@@ -47,6 +49,79 @@ class ImageController extends Controller
             }
             // FIXME: надо удалять не найденные картинки из БД
         }
+    }
+
+    public function upload(UploadRequest $request, $albumHash)
+    {
+        $album = Album::getByHash($albumHash);
+        $files = $request->file('images');
+        $path = "images$album->path";
+        $allowedExts = array_column(ImageExtensionsEnum::cases(), 'value');
+        $allowedExitsImploded = implode(',', $allowedExts);
+
+        $responses = [];
+        foreach ($files as $file) {
+            $fileName = $file->getClientOriginalName();
+            $fileExt  = $file->extension();
+
+            // Валидация файла
+            $validator = Validator::make(['file' => $file], [
+                'file' => ['mimes:'. $allowedExitsImploded],
+            ]);
+            if ($validator->fails()) {
+                // Сохранение плохого ответа API
+                $responses['errored'][] = [
+                    'name'    => $fileName,
+                    'message' => $validator->errors(),
+                ];
+                continue;
+            }
+
+            // Проверка существования того же файла
+            $imageHash = md5(File::get($file->getRealPath()));
+            $imageFounded = false;
+            try {
+                Image::getByHash($albumHash, $imageHash);
+            } finally {
+                $imageFounded = true;
+            }
+            if ($imageFounded) {
+                // Сохранение плохого ответа API
+                $responses['errored'][] = [
+                    'name'    => $fileName,
+                    'message' => "Image with md5 hash \"$imageHash\" already exist in this album",
+                ];
+                continue;
+            }
+
+            // Наименование повторяющихся
+            $fileNameNoExt = basename($fileName, ".$fileExt");
+            $num = 1;
+            while (Storage::exists("$path$fileName")) {
+                $fileName = "$fileNameNoExt ($num).$fileExt";
+                $num++;
+            }
+
+            // Сохранение файла в хранилище
+            $file->storeAs($path,$fileName);
+
+            $sizes = getimagesize(Storage::path($path.$fileName));
+
+            // Сохранение записи в БД
+            $imageDB = Image::create([
+                'name' => $fileName,
+                'hash' => $imageHash,
+                'date' => Carbon::createFromTimestamp(Storage::lastModified($path.$fileName)),
+                'size' => Storage::size($path.$fileName),
+                'width'  => $sizes[0],
+                'height' => $sizes[1],
+                'album_id' => $album->id,
+            ]);
+
+            // Сохранение успешного ответа API
+            $responses['successful'][] = ImageResource::make($imageDB);
+        }
+        return response($responses);
     }
 
     public function showAll(AlbumImagesRequest $request, $albumHash)
